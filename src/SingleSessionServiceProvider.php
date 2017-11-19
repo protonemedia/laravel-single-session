a617d32fede69857afcb2ee1aaac967045dff14c
pascalbaljet <pascalbaljet@mp-pb.local>
2017-11-19 22:01:29 +0100

    Added event listeners to service provider

--- src/SingleSessionServiceProvider.php
+++ src/SingleSessionServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace Pbmedia\SingleSession;
 
+use Illuminate\Auth\Events\Login;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use Pbmedia\SingleSession\Listeners\DestroyPreviousUserSession;
+use Pbmedia\SingleSession\Listeners\StoreUserSessionId;
 
 class SingleSessionServiceProvider extends ServiceProvider
 {
@@ -20,6 +24,10 @@ class SingleSessionServiceProvider extends ServiceProvider
                 __DIR__ . '/../database/migrations/add_session_id_to_users_table.php.stub' =>
                 database_path('migrations/' . date('Y_m_d_His', time()) . '_add_session_id_to_users_table.php'),
             ], 'migrations');
+        }
+
+        foreach ([DestroyPreviousUserSession::class, StoreUserSessionId::class] as $listener) {
+            Event::listen(Login::class, $listener);
         }
     }
 
